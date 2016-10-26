198b5bc9c82754ec0788ddd7181b74efc02ed7ac
幕希Lin <2103887953@qq.com>
2016-10-26 21:35:40 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ var router = express.Router();
 var redis = require('redis')
 var client = redis.createClient(6379, 'localhost')
 
-Object.prototype.toString = function(){
-	return JSON.stringify(this)
-}
+// Object.prototype.toString = function(){
+// 	return JSON.stringify(this)
+// }
 
-client.set('a', {a: 1, b: 2})
-client.get('a', function( v) {
-	console.log('redis get hello',  v, typeof v)
+// client.set('a', {a: 1, b: 2})
+// client.get('a', function( v) {
+// 	console.log('redis get hello',  v, typeof v)
+// })
+
+// client.lpush('list', '1')
+// client.rpush('list', 'b')
+// client.rpush('list', 'c')
+// client.rpush('list', 'd')
+client.lrange('list', 0, -1, (list)=>{
+	console.log(list)
 })
+
 module.exports = router;
