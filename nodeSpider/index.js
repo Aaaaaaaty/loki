5e99e4fae26b88c0788f87e0ad88c437a740b875
幕希Lin <2103887953@qq.com>
2016-11-15 09:56:09 +0800

    add .gitignore

--- nodeSpider/index.js
+++ nodeSpider/index.js
@@ -1,13 +1,11 @@
 var cheerio = require('cheerio')  
 var superagent = require('superagent')  
 var fs = require('fs')
-var path = require('./urlRandom.js')
+// var path = require('./urlRandom.js')
 // var url = path.urlCreate('http://movie.douban.com/subject/')
-var url = 
+var url = 'http://weibo.com/1890157630/EhlKLocAX?type=comment#_rnd1479117806171'
 var infoResult = []
-
-// for(var i = 11000000; i < 0; i--) {
-    superagent.get(url[i])
+superagent.get(url)
     .end(function(err, res) {
         if(err) {
             next(err)
@@ -15,31 +13,31 @@ var infoResult = []
         var $ = cheerio.load(res.text, {
             xmlMode: true
         })
+        console.log($.html())
         var d = new Date()
         var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate()) // 取得爬取的日期
         var infoResult = { // 设定爬取的json数组
             date: date,
             info: []
         }
-        var movieName = '电影名:' + $('#content h1 span').filter(function() {
-            return $(this).attr('property') == 'v:itemreviewed'
-        }).text()
-        var directories = '- 导演名：' + $('#info span a').filter(function() {
-            return $(this).attr('rel') == 'v:directedBy'
-        }).text()
-        var starName = '- 演员名：' + $('#info span a').filter(function() {
-            return $(this).attr('rel') == 'v:starring'
-        }).text()
-        var result = movieName + '\r\n' + directories + '\r\n' + starName + '\r\n' 
-        infoResult.info.push({
-            movieName: movieName,
-            directories: directories,
-            starName: starName
+        $('.WB_text a').each(function() {
+            infoResult.info.push({
+                'user': $(this).text
+            })
         })
-        console.log('i')
-        console.log(infoResult.info[0].movieName)
+        var directories = '- 评论' + $('.WB_text').text()
+        // var starName = '- 演员名：' + $('#info span a').filter(function() {
+        //     return $(this).attr('rel') == 'v:starring'
+        // }).text()
+        // var result = movieName + '\r\n' + directories + '\r\n'
+        // infoResult.info.push({
+        //     movieName: movieName,
+        //     directories: directories,
+        //     // starName: starName
+        // })
+        // console.log(infoResult.info[0].movieName)
+        console.log(infoResult)
     })
-// }
 
 
 // fs.writeFile('dbmovie.txt', data, 'utf-8', function(err){
